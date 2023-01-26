1afddd759267a361cbd3b0946e26e5c1d5555561
unknown <Saba-Maisa>
2023-01-26 21:42:53 +0400

    movies

--- jsFile/index.js
+++ jsFile/index.js
@@ -1,4 +1,3 @@
-import { movieApi } from "./vdeoApi.js";
 
 ///// slider//////////////
 const modal = document.querySelector(".modal");
@@ -280,3 +279,34 @@ function modalBox(element) {
 
   movieApi(element.id);
 }
+
+const api = "78828dca7949b70ca50313e4d49660d1";
+const videoYoutube = document.querySelector(".video-youtube");
+
+function movieApi(movie) {
+  axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${movie}/videos?api_key=${api}&language=en-US`
+    )
+    .then(function (response) {
+      let responseMoves = response.data.results[0];
+
+      const movieDiv = document.createElement("div");
+      const movieRemove = document.querySelectorAll(".movieVideoRemove");
+      movieDiv.setAttribute("class", "movieVideoRemove");
+      movieRemove.forEach((item) => {
+        item.remove();
+      });
+
+      movieDiv.innerHTML = `
+            <iframe src="https://www.youtube.com/embed/${responseMoves.key}"></iframe>  
+                   `;
+      videoYoutube.appendChild(movieDiv);
+    })
+
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+
