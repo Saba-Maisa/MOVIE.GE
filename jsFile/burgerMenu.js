b65e163fb7dd980c0c71bd9f073aafa423a0a015
unknown <Saba-Maisa>
2023-01-26 23:19:54 +0400

    movies

--- jsFile/burgerMenu.js
+++ jsFile/burgerMenu.js
@@ -1,8 +1,8 @@
-const burger = document.getElementById("burger");
-const navMenu = document.getElementById("ulList");
+// const burger = document.getElementById("burger");
+// const navMenu = document.getElementById("ulList");
 
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
+// burger.addEventListener("click", () => {
+//   burger.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+// });
 
--- jsFile/index.js
+++ jsFile/index.js
@@ -277,37 +277,39 @@ function modalBox(element) {
 
   modalContainerInfo.appendChild(modalInfo);
 
-  movieApi(element.id);
+  // movieApi(element.id);
 }
 
-const api = "78828dca7949b70ca50313e4d49660d1";
-const videoYoutube = document.querySelector(".video-youtube");
+// const api = "78828dca7949b70ca50313e4d49660d1";
+// const videoYoutube = document.querySelector(".video-youtube");
 
 
 
-function movieApi(movie) {
-  axios
-    .get(`https://api.themoviedb.org/3/movie/${movie}/videos?api_key=${api}&language=en-US`
-    )
-    .then(function (response) {
-      let responseMoves = response.data.results[0];
+// function movieApi(movie) {
+//   axios
+//     .get(`https://api.themoviedb.org/3/movie/${movie}/videos?api_key=${api}&language=en-US`
+//     )
+//     .then(function (response) {
+//       let responseMoves = response.data.results[0];
 
-      const movieDiv = document.createElement("div");
-      const movieRemove = document.querySelectorAll(".movieVideoRemove");
-      movieDiv.setAttribute("class", "movieVideoRemove");
-      movieRemove.forEach((item) => {
-        item.remove();
-      });
+//       console.log(response)
 
-      movieDiv.innerHTML = `
-            <iframe src="https://www.youtube.com/embed/${responseMoves.key}"></iframe>  
-                   `;
-      videoYoutube.appendChild(movieDiv);
-    })
+//       const movieDiv = document.createElement("div");
+//       const movieRemove = document.querySelectorAll(".movieVideoRemove");
+//       movieDiv.setAttribute("class", "movieVideoRemove");
+//       movieRemove.forEach((item) => {
+//         item.remove();
+//       });
 
-    .catch(function (error) {
-      console.log(error);
-    });
-}
+//       movieDiv.innerHTML = `
+//             <iframe src="https://www.youtube.com/embed/${responseMoves.key}"></iframe>  
+//                    `;
+//       videoYoutube.appendChild(movieDiv);
+//     })
+
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// }
 
 
